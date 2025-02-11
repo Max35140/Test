ccdb91e7f8ace707d669618e214db0c2a96c4d8d
Max35140 <max.burger@stud.tu-darmstadt.de>
2025-02-11 21:03:24 +0100

    Update task.c

--- task.c
+++ task.c
@@ -5,6 +5,11 @@ void task_function(int id) {
   printf("Task %d is being executed by thread %d\n", id, omp_get_thread_num());
 }
 
+void test_function(int id) {
+  int i;
+  i = id + 2;
+}
+
 int main() {
   omp_set_num_threads(4);
   #pragma omp parallel 
@@ -14,7 +19,7 @@ int main() {
       for (int i = 0; i < 10; i++) {
         #pragma omp task
         {
-          task_function(i);
+          test_function(i);
         }
       }
     }
