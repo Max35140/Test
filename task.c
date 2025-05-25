a55ec1ba121b0d76d66efffcba79bb07dbbc9c1d
Max35140 <max.burger@stud.tu-darmstadt.de>
2025-05-25 21:37:46 +0200

    Update task.c

--- task.c
+++ task.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <omp.h>
 
-int task_funktion(int id) {
-  printf("Task %d is being executed by thread %d\n", id, omp_get_thread_num());
+int fib(int n)
+{
+    if (n < 2)
+        return n;
+
+    int i, j;
+
+    #pragma omp task default(none) shared(i) firstprivate(n)
+    i = fib(n-1);
+
+    #pragma omp task default(none) shared(j) firstprivate(n)
+    j = fib(n-2);
+
+    #pragma omp taskwait
+
+    return i + j;
 }
 
-
 int main() {
-  omp_set_num_threads(4);
-  #pragma omp parallel 
-  {
-    #pragma omp master
-    {
-      for (int i = 0; i < 10; i++) {
-        #pragma omp task
-        {
-          task_funktion(i);
-        }
-        #pragma omp task
-        {
-          task_funktion(i);
-        }
-      }
-    }
-  }
-  return 0;
+    #pragma omp parallel
+    #pragma omp single
+    printf("fib(10) = %d\n", fib(10));
+
+    return 0;
 }
